0a016bab3aa002a9cea233b0443028c6102f29ad
gamingwithmaddie <piperjeannelson@gmail.com>
2026-06-04 13:35:45 -0400

    Simplify client.lua - use local commands instead of server events

--- client.lua
+++ client.lua
@@ -1,24 +1,30 @@
+-- Simple client-side /do command handler
 local floatingText = nil
-local floatingTextCoords = nil
 
--- Listen for server events
-RegisterNetEvent('do:createFloatingText', function(text)
+-- Register command on client
+RegisterCommand('do', function(source, args, rawCommand)
+    if #args == 0 then
+        TriggerEvent('chat:addMessage', {
+            color = {255, 0, 0},
+            multiline = true,
+            args = {"ERROR", "Usage: /do [text]"}
+        })
+        return
+    end
+
+    local text = table.concat(args, " ")
     floatingText = text
-    local playerPed = PlayerPedId()
-    local waistBone = GetPedBoneIndex(playerPed, 0x0cc49bbe) -- Waist bone SKEL_Spine2
-    floatingTextCoords = GetWorldPositionOfEntityBone(playerPed, waistBone)
     
     TriggerEvent('chat:addMessage', {
         color = {0, 255, 0},
         multiline = true,
         args = {"DO", text}
     })
-end)
+end, false)
 
-RegisterNetEvent('do:removeFloatingText', function()
+RegisterCommand('removedo', function(source, args, rawCommand)
     if floatingText ~= nil then
         floatingText = nil
-        floatingTextCoords = nil
         TriggerEvent('chat:addMessage', {
             color = {0, 255, 0},
             multiline = true,
@@ -31,7 +37,7 @@ RegisterNetEvent('do:removeFloatingText', function()
             args = {"ERROR", "No floating text to remove"}
         })
     end
-end)
+end, false)
 
 -- Render the floating text on screen
 Citizen.CreateThread(function()
@@ -50,21 +56,21 @@ end)
 
 function DrawText3D(x, y, z, text)
     local onScreen, _x, _y = World3dToScreen2d(x, y, z)
-    local px, py, pz = table.unpack(GetGameplayCamCoords())
-    local dist = #(vector3(x, y, z) - vector3(px, py, pz))
-    
-    local scale = (1 / dist) * 2
-    local fov = (1 / GetGameplayCamFov()) * 100
-    local scale = scale * fov
     
     if onScreen then
-        SetTextScale(0.0 * scale, 0.55 * scale)
+        local px, py, pz = table.unpack(GetGameplayCamCoords())
+        local dist = #(vector3(x, y, z) - vector3(px, py, pz))
+        
+        local scale = (1 / dist) * 2
+        local fov = (1 / GetGameplayCamFov()) * 100
+        local finalScale = scale * fov
+        
+        SetTextScale(0.0 * finalScale, 0.55 * finalScale)
         SetTextFont(4)
         SetTextProportional(1)
         SetTextColour(255, 255, 255, 215)
         SetTextDropshadow(0, 0, 0, 0, 255)
         SetTextEdge(2, 0, 0, 0, 150)
-        SetTextDropShadow()
         SetTextOutline()
         BeginTextCommandDisplayText("STRING")
         AddTextComponentString(text)
