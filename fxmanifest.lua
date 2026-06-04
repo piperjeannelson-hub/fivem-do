f8a01a9f07b20b17bff921c67c3d6862891f8c6e
gamingwithmaddie <piperjeannelson@gmail.com>
2026-06-04 13:35:24 -0400

    Fix fxmanifest - remove qb-core import (not needed for basic commands)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,10 +5,6 @@ author 'piperjeannelson-hub'
 description 'QBCore /do command - Create floating text at ped waist'
 version '1.0.0'
 
-shared_scripts {
-    '@qb-core/import.lua',
-}
-
 client_scripts {
     'client.lua',
 }
