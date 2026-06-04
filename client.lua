5aa46f532d36c564ddc0d9fb12e468fb300e73e3
gamingwithmaddie <piperjeannelson@gmail.com>
2026-06-04 13:41:21 -0400

    Fix DrawText3D - improve 3D text rendering

--- client.lua
+++ client.lua
@@ -45,35 +45,37 @@ Citizen.CreateThread(function()
         Wait(0)
         if floatingText ~= nil then
             local playerPed = PlayerPedId()
-            local waistBone = GetPedBoneIndex(playerPed, 0x0cc49bbe)
-            local waistCoords = GetWorldPositionOfEntityBone(playerPed, waistBone)
-            
-            -- Display 3D text above the waist
-            DrawText3D(waistCoords.x, waistCoords.y, waistCoords.z + 0.7, floatingText)
+            if playerPed ~= 0 then
+                local waistBone = GetPedBoneIndex(playerPed, 0x0cc49bbe) -- Waist bone
+                if waistBone ~= 0 then
+                    local waistCoords = GetWorldPositionOfEntityBone(playerPed, waistBone)
+                    DrawText3D(waistCoords.x, waistCoords.y, waistCoords.z + 1.0, floatingText)
+                end
+            end
         end
     end
 end)
 
 function DrawText3D(x, y, z, text)
-    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
+    local camCoords = GetGameplayCamCoords()
+    local distance = #(vector3(x, y, z) - camCoords)
     
-    if onScreen then
-        local px, py, pz = table.unpack(GetGameplayCamCoords())
-        local dist = #(vector3(x, y, z) - vector3(px, py, pz))
-        
-        local scale = (1 / dist) * 2
-        local fov = (1 / GetGameplayCamFov()) * 100
-        local finalScale = scale * fov
-        
-        SetTextScale(0.0 * finalScale, 0.55 * finalScale)
-        SetTextFont(4)
-        SetTextProportional(1)
-        SetTextColour(255, 255, 255, 215)
-        SetTextDropshadow(0, 0, 0, 0, 255)
-        SetTextEdge(2, 0, 0, 0, 150)
-        SetTextOutline()
-        BeginTextCommandDisplayText("STRING")
-        AddTextComponentString(text)
-        EndTextCommandDisplayText(_x, _y)
-    end
+    local onScreen, screenX, screenY = World3dToScreen2d(x, y, z)
+    if not onScreen then return end
+    
+    -- Scale based on distance
+    local scale = (1.0 / distance) * 2.0
+    local fov = (1.0 / GetGameplayCamFov()) * 100.0
+    local finalScale = scale * fov
+    
+    SetTextScale(0.0 * finalScale, 0.5 * finalScale)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 255)
+    SetTextOutline()
+    SetTextCentre(1)
+    
+    BeginTextCommandDisplayText("STRING")
+    AddTextComponentString(text)
+    EndTextCommandDisplayText(screenX, screenY)
 end
