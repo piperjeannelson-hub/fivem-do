5059451cf1874cac6d64b9e157062c8730869a36
gamingwithmaddie <piperjeannelson@gmail.com>
2026-06-04 13:13:10 -0400

    Add fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'piperjeannelson-hub'
+description 'QBCore /do command - Create floating text at ped waist'
+version '1.0.0'
+
+shared_scripts {
+    '@qb-core/import.lua',
+}
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua',
+}
